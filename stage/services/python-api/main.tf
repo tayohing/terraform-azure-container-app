756ff994eb8f852959c428b53910ad648637a37c
Tayo Hing <tayohinginfo@gmail.com>
2026-06-06 22:58:27 +0100

    Added secret and registry passwords to main.tf Container App block

--- stage/services/python-api/main.tf
+++ stage/services/python-api/main.tf
@@ -44,6 +44,15 @@ resource "azurerm_container_app" "ca-containerapp-stage" {
   resource_group_name          = azurerm_resource_group.main.name
   container_app_environment_id = azurerm_container_app_environment.cae-containerapp-stage.id
   revision_mode                = "Single"
+  secret {
+    name  = "acr-password"
+    value = azurerm_container_registry.acrcontainerapp.admin_password
+  }
+  registry {
+    server   = azurerm_container_registry.acrcontainerapp.login_server
+    username = azurerm_container_registry.acrcontainerapp.admin_username
+    password_secret_name = "acr-password"
+  }
   template {
     container {
       name   = "python-api"
