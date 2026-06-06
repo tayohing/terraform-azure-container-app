953cc023cebb54f86ae111e527efd60c02a2e5ae
Tayo Hing <tayohinginfo@gmail.com>
2026-06-06 22:48:46 +0100

    Updated main.tf ACR block with admin_enabled = true

--- stage/services/python-api/main.tf
+++ stage/services/python-api/main.tf
@@ -30,6 +30,7 @@ resource "azurerm_container_registry" "acrcontainerapp" {
   resource_group_name = azurerm_resource_group.main.name
   location            = var.location
   sku                 = "Basic"
+  admin_enabled       = true
 }
 
 resource "azurerm_container_app_environment" "cae-containerapp-stage" {
